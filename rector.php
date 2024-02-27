51dc688b37e92665d953e9e9f4537399474313d4
Rodrigo Cardoso <erinofgok@gmail.com>
2024-02-27 11:32:33 -0300

    add config rector

---
+++ rector.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withFileExtensions(['php', 'phtml'])
+    ->withPaths([
+        __DIR__ . '/',
+    ])
+    ->withPhpSets(php81: true)
+    ->withPreparedSets(codeQuality: true);
+
